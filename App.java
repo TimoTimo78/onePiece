d4b840704a5cd89c51aa07213b0a4d29bd9a95c9
Cedric <116574782+TimoTimo78@users.noreply.github.com>
2023-05-17 18:48:12 +0200

    crétaion de DanaSuperGirl

--- App.java
+++ App.java
@@ -7,11 +7,16 @@ public class App {
         
         // loffi pirate
         // jimbe pirate homme poisson
+        // danaSpG SuperGirl
 
         Pirate p = new Pirate("Loffi");
         p.move(32, 42);
         HommePoisson hp = new HommePoisson("Jimbe");
         hp.move(45, 24);
         hp.swim(5);
+        SuperGirl sp = new SuperGirl("DanaSpG");
+        sp.move(25, 40);
+        sp.swim(10, 20, 40);
+        sp.fly(24, 30, 25);
     }
 }
---
+++ com/perso/SuperGirl.java
@@ -0,0 +1,28 @@
+package com.perso;
+
+public class SuperGirl extends HommePoisson {
+    private int posZ;
+    private int posX;
+    private int posY;
+    public SuperGirl(String name) {
+        super(name);
+        
+
+    }
+    public void fly(int x, int y, int z)
+    {
+        this.posX = x;
+        this.posY = y;
+        this.posZ = z;
+
+        System.out.println("SuperGirl vole en X :" + this.posX + " et en Y : " + this.posY + " et en Z : " + this.posZ);
+    }
+    public void swim(int x, int y, int z)
+    {
+        this.posX = x;
+        this.posY = y;
+        this.posZ = z;
+        
+        System.out.println("SuperGirl nage en X :" + this.posX + " et en Y : " + this.posY + " et en Z : " + this.posZ);
+    }
+}
